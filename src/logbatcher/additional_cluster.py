0fbe090d660206878aaa65ef8f293a767c090b74
bolu61 <bolu61@zjc.dev>
2025-11-29 11:09:02 -0500

    Fix regex escaping in log format processing

--- src/logbatcher/additional_cluster.py
+++ src/logbatcher/additional_cluster.py
@@ -63,7 +63,7 @@ def clean(s):
     log_format = re.sub(r'[0-9A-Za-z, ]+', '', s)
     unique_chars = list(set(log_format))
     sorted_string = ''.join(sorted(unique_chars))
-    s = re.sub(':|\(|\)|=|,|"|\{|\}|@|$|\[|\]|\||;|\.?!', ' ', s)
+    s = re.sub(r':|\(|\)|=|,|"|\{|\}|@|$|\[|\]|\||;|\.?!', ' ', s)
     s = " ".join([word for word in s.strip().split() if not bool(re.search(r'\d', word))])
     # trantab = str.maketrans(dict.fromkeys(list(string.punctuation)))
     return s, sorted_string
