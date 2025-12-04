58eb8b85636d5c42e78feca5170cd63082eca2ba
bolu61 <bolu61@zjc.dev>
2025-12-04 12:02:03 -0500

    Use re2 to speedup pattern matching

--- src/logbatcher/matching.py
+++ src/logbatcher/matching.py
@@ -1,42 +1,22 @@
 import re
+import re2
 from logbatcher.cluster import Cluster
-
-import signal
-
-class TimeoutException(Exception):
-    pass
-
-def timeout_handler(signum, frame):
-    raise TimeoutException()
-
-def safe_search(pattern, string, timeout=1):
-    signal.signal(signal.SIGALRM, timeout_handler)
-    signal.alarm(timeout)
-    try:
-        result = re.search(pattern, string)
-    except TimeoutException:
-        result = None
-    finally:
-        signal.alarm(0)
-    return result
 
 
 # @timeout(10)
 def extract_variables(log, template):
-    log = re.sub(r'\s+', ' ', log.strip()) # DS
+    log = re.sub(r"\s+", " ", log.strip())  # DS
     pattern_parts = template.split("<*>")
     pattern_parts_escaped = [re.escape(part) for part in pattern_parts]
     regex_pattern = "(.*?)".join(pattern_parts_escaped)
-    regex = "^" + regex_pattern + "$"  
-    # matches = re.search(regex, log)
-    matches = safe_search(regex, log, 1)
+    matches = re2.match(regex_pattern, log)
     if matches:
         return matches.groups()
     else:
         return None
 
-def matches_template(log, cached_pair):
 
+def matches_template(log, cached_pair):
     reference_log = cached_pair[0]
     template = cached_pair[1]
 
@@ -46,9 +26,9 @@ def matches_template(log, cached_pair):
 
     try:
         groups = extract_variables(log, template)
-    except:
+    except Exception:
         groups = None
-    if groups == None:
+    if groups is None:
         return None
 
     # consider the case where the varaible is empty
@@ -56,21 +36,19 @@ def matches_template(log, cached_pair):
     for index, part in enumerate(template.split("<*>")):
         parts.append(part)
         if index < len(groups):
-            if groups[index] == '':
-                parts.append('')
+            if groups[index] == "":
+                parts.append("")
             else:
-                parts.append('<*>')
+                parts.append("<*>")
 
-    return ''.join(parts)
-
+    return "".join(parts)
 
 
 def prune_from_cluster(template, cluster):
-
     new_cluster = Cluster()
     logs, indexs = cluster.logs, cluster.indexs
     for log, index in zip(logs, indexs):
-        if extract_variables(log, template) == None:
+        if extract_variables(log, template) is None:
             new_cluster.append_log(log, index)
     if new_cluster.size != 0:
         old_logs = [log for log in logs if log not in new_cluster.logs]
